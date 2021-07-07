fe03fb4badf1d96301bae3e0da31482511c21ae0
Neeraj Maurya <neeraj.maurya@mobcoder.com>
2021-07-07 11:17:59 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Mobcoder on 07/07/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
